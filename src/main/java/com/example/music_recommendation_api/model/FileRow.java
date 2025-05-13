b80e14d0d2521c4cf2afed2ecb72b0e01343a143
jmc <120180071+sector-11@users.noreply.github.com>
2025-05-13 15:38:04 +0100

    feat: add filerow class to model rows in data csv

---
+++ src/main/java/com/example/music_recommendation_api/model/FileRow.java
@@ -0,0 +1,60 @@
+package com.example.music_recommendation_api.model;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity @Getter @Setter
+public class FileRow {
+    private final int lineNumber;
+    private String trackId;
+    private String artists;
+    private String albumName;
+    private String trackName;
+    private int popularity;
+    private int durationMs;
+    private boolean explicit;
+    private float danceability;
+    private float energy;
+    private int key;
+    private float loudness;
+    private boolean mode;
+    private float speechiness;
+    private float acousticness;
+    private double instrumentalness;
+    private float liveness;
+    private float valence;
+    private float tempo;
+    private int timeSignature;
+    private String trackGenre;
+
+    public FileRow(int lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+
+    @Override
+    public String toString() {
+        return lineNumber + " {" +
+                "trackId='" + trackId + '\'' +
+                ", artists='" + artists + '\'' +
+                ", albumName='" + albumName + '\'' +
+                ", trackName='" + trackName + '\'' +
+                ", popularity=" + popularity +
+                ", durationMs=" + durationMs +
+                ", explicit=" + explicit +
+                ", danceability=" + danceability +
+                ", energy=" + energy +
+                ", key=" + key +
+                ", loudness=" + loudness +
+                ", mode=" + mode +
+                ", speechiness=" + speechiness +
+                ", acousticness=" + acousticness +
+                ", instrumentalness=" + instrumentalness +
+                ", liveness=" + liveness +
+                ", valence=" + valence +
+                ", tempo=" + tempo +
+                ", timeSignature=" + timeSignature +
+                ", trackGenre='" + trackGenre + '\'' +
+                '}';
+    }
+}
