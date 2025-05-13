72898d8ed2d1f4a44400a6c14cf3df2a106842f6
jmc <120180071+sector-11@users.noreply.github.com>
2025-05-13 16:27:23 +0100

    feat: add genre model object

---
+++ src/main/java/com/example/music_recommendation_api/model/Genre.java
@@ -0,0 +1,16 @@
+package com.example.music_recommendation_api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity @Getter @Setter
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String genre;
+}
