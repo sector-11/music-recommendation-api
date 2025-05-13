b6e24743c511480dbc2a22b8a9e83f33fe4cc60d
jmc <120180071+sector-11@users.noreply.github.com>
2025-05-13 15:26:23 +0100

    add initial files

---
+++ src/main/java/com/example/music_recommendation_api/MusicRecommendationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.music_recommendation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicRecommendationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicRecommendationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/music_recommendation_api/MusicRecommendationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.music_recommendation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicRecommendationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
