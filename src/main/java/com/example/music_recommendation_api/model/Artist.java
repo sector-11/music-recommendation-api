7b7c494df50117063e0eb54227951ce81e5822df
jmc <120180071+sector-11@users.noreply.github.com>
2025-05-13 16:40:20 +0100

    feat: enable many-to-many mapping between artist and album

--- src/main/java/com/example/music_recommendation_api/model/Artist.java
+++ src/main/java/com/example/music_recommendation_api/model/Artist.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity @Getter @Setter
 @Table(name = "artists")
 public class Artist {
@@ -11,4 +13,7 @@ public class Artist {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+
+    @ManyToMany(mappedBy = "artists")
+    List<Album> albums;
 }
