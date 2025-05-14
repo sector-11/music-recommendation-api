739c49565dcedee011e798f3000743b00fed9a03
jmc <120180071+sector-11@users.noreply.github.com>
2025-05-15 00:10:28 +0100

    feat: add dataseeder component to check if there's data in the db on boot and then run file reader service if none found

---
+++ src/main/java/com/example/music_recommendation_api/util/DataSeeder.java
@@ -0,0 +1,35 @@
+package com.example.music_recommendation_api.util;
+
+import com.example.music_recommendation_api.model.FileRow;
+import com.example.music_recommendation_api.service.DataFileReader;
+import com.example.music_recommendation_api.service.SongService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DataSeeder {
+    @Autowired
+    private final SongService songService;
+    @Autowired
+    private final DataFileReader reader;
+
+
+    public DataSeeder(SongService songService, DataFileReader reader) {
+        this.songService = songService;
+        this.reader = reader;
+    }
+
+    @Bean
+    public CommandLineRunner checkAndSeed() {
+        if (!songService.getAllSongs().isEmpty()) {
+            return args -> {/*do nothing*/};
+        }
+        List<FileRow> file = reader.readFile();
+
+        return args -> {reader.parseAll(file);};
+    }
+}
