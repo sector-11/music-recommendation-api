3a409f8fc84f6cf7909694f955ad15014a3fdf64
jmc <120180071+sector-11@users.noreply.github.com>
2025-05-14 23:21:58 +0100

    feat: add method to parse file and persist information to active database

--- src/main/java/com/example/music_recommendation_api/service/DataFileReader.java
+++ src/main/java/com/example/music_recommendation_api/service/DataFileReader.java
@@ -1,25 +1,43 @@
 package com.example.music_recommendation_api.service;
 
-import com.example.music_recommendation_api.model.FileRow;
-import com.example.music_recommendation_api.model.Song;
+import com.example.music_recommendation_api.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
+@Service
 public class DataFileReader {
+    private final String FILEPATH = "src/main/resources/testdata.csv";
     private final Pattern regex;
-    private final String filepath;
     private BufferedReader reader;
+    private final Map<String, Artist> artists = new HashMap<>();
+    private final Map<String, Album> albums = new HashMap<>();
+    private final Map<String, Genre> genres = new HashMap<>();
 
-    public DataFileReader(String filepath) {
+    @Autowired
+    private final AlbumService albumService;
+    @Autowired
+    private final ArtistService artistService;
+    @Autowired
+    private final GenreService genreService;
+    @Autowired
+    private final SongService songService;
+
+    public DataFileReader(AlbumService albumService, ArtistService artistService, GenreService genreService, SongService songService) {
+        this.albumService = albumService;
+        this.artistService = artistService;
+        this.genreService = genreService;
+        this.songService = songService;
         this.regex = Pattern.compile(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
         this.reader = null;
-        this.filepath = filepath;
     }
 
 
@@ -28,7 +46,7 @@ public class DataFileReader {
         String line = "";
 
         try {
-            reader = new BufferedReader(new FileReader(filepath));
+            reader = new BufferedReader(new FileReader(FILEPATH));
             reader.readLine(); //skip first line
 
             while ((line = reader.readLine()) != null) {
@@ -89,5 +107,48 @@ public class DataFileReader {
         song.setLoudness(row.getLoudness());
 
         return song;
+    }
+
+    public void parseAll(List<FileRow> rows) {
+        for (FileRow row : rows) {
+            Song song = songService.addSong(this.parseSong(row));
+            String[] songArtists = row.getArtists().split(";");
+
+            if(!albums.containsKey(row.getAlbumName())) {
+                albums.put(row.getAlbumName(), albumService.addAlbum(new Album()));
+                albums.get(row.getAlbumName()).setName(row.getAlbumName());
+            }
+
+            Album album = albums.get(row.getAlbumName());
+            album.getSongs().add(song);
+            song.getAlbums().add(album);
+
+            for (String artistName : songArtists) {
+                if (!artists.containsKey(artistName)) {
+                    artists.put(artistName, artistService.addArtist(new Artist()));
+                    artists.get(artistName).setName(artistName);
+                }
+
+                Artist artist = artists.get(artistName);
+                artist.getSongs().add(song);
+                song.getArtists().add(artist);
+                album.getArtists().add(artist);
+                artistService.addArtist(artist);
+            }
+
+            if(!genres.containsKey(row.getTrackGenre())) {
+                genres.put(row.getTrackGenre(), genreService.addGenre(new Genre()));
+                genres.get(row.getTrackGenre()).setGenre(row.getTrackGenre());
+            }
+
+            Genre genre = genres.get(row.getTrackGenre());
+            genre.setGenre(row.getTrackGenre());
+            genre.getSongs().add(song);
+            song.setGenre(genre);
+
+            songService.addSong(song);
+            albumService.addAlbum(album);
+            genreService.addGenre(genre);
+        }
     }
 }
