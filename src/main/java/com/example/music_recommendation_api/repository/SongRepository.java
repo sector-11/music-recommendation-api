c371d8528c4b08662f92be706ddc91fb9a4dfa68
Megan O <meganoconnor2005@gmail.com>
2025-05-15 11:23:47 +0100

    removing commented out code

--- src/main/java/com/example/music_recommendation_api/repository/SongRepository.java
+++ src/main/java/com/example/music_recommendation_api/repository/SongRepository.java
@@ -1,9 +1,7 @@
 package com.example.music_recommendation_api.repository;
 
 import com.example.music_recommendation_api.model.Song;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/example/music_recommendation_api/service/SongService.java
+++ src/main/java/com/example/music_recommendation_api/service/SongService.java
@@ -46,18 +46,10 @@ public class SongService {
     }
 
     public ArrayList<Song> getSongByGenre(Integer genre_id){
-//        ArrayList<Song> foundSongsByGenre = new ArrayList<>();
-//        Iterable<Song> foundSongs = songRepository.findAll();
-//        foundSongs.forEach(song -> {foundSongsByGenre.add(song);});
-//        return foundSongsByGenre;
         return songRepository.findSongByGenreOrderByTrack_NameAsc(genre_id).orElseThrow();
     }
 
     public ArrayList<Song> getSongByArtist(Integer artist_id){
-//        ArrayList<Song> foundSongsByArtist = new ArrayList<>();
-//        Iterable<Song> foundSongs = songRepository.findAll();
-//        foundSongs.forEach(song -> {foundSongsByArtist.add(song);});
-//        return foundSongsByArtist;
         return songRepository.findSongByArtistOrderByTrack_NameAsc(artist_id).orElseThrow();
     }
 
